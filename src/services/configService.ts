2a01c88b1d2260a0235053e0f04260287c1c079f
Ryan Fahlepy Sinaga <112138816+ryanfahlepy@users.noreply.github.com>
2023-09-22 10:18:40 +0700

    Update configService.ts

--- src/services/configService.ts
+++ src/services/configService.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://192.168.87.156:5000/api';
-export const WS_BASE_URL = 'ws://192.168.87.156:5000/ws';
+export const API_BASE_URL = 'http://3.27.88.233:5000/api';
+export const WS_BASE_URL = 'ws://3.27.88.233:5000/ws';
 let token = '';
 
 export const getToken = () => token
